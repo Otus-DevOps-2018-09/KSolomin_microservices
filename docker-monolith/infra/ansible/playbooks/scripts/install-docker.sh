82b9dc6a6cb93b09b3c2abf18d8767fe60fc308b
Konstantin Solomin <konstantin.solomin@omnigon.com>
2018-12-09 19:01:28 +0300

    Docker-2 (Homework #13)

---
+++ docker-monolith/infra/ansible/playbooks/scripts/install-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+apt-get update
+apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker
+apt-get install -y python-pip
